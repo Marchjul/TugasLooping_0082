879c246ba475a69e42d5968b3f1ee96d604911a4
Marchjul <marchellogiovani28@gmail.com>
2026-03-31 18:08:39 +0700

    fungsi menampilkan menu

--- Praktikum.cpp
+++ Praktikum.cpp
@@ -4,3 +4,13 @@ using namespace std;
 // Variabel Global
 int angka_input;
 int pilihan_menu;
+
+// Fungsi untuk menampilkan menu
+void tampilMenu() {
+    cout << "\n=== PROGRAM CEK BILANGAN ===" << endl;
+    cout << "1. Cek Bilangan Prima" << endl;
+    cout << "2. Cek Bilangan Fibonacci" << endl;
+    cout << "0. Keluar" << endl;
+    cout << "Masukkan pilihan Anda: ";
+    cin >> pilihan_menu;
+}
